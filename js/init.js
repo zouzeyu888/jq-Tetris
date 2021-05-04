dd12e30450cb8f8dd47990441f5395642ca3b0a8
zouzeyu888 <1198973046@qq.com>
2021-05-04 17:18:56 +0800

    代码重构

---
+++ js/init.js
@@ -0,0 +1,55 @@
+const SQUARE_WIDTH = 20; // 方格宽度
+
+const SQUARE_COLOR = "red"; // 方格颜色
+
+const BOARD_ROW = 20; // 棋盘行数
+
+const BOARD_COLUMN = 10; // 棋盘列数
+
+const BOARD_COLOR = "orange";
+
+// 方块形状
+const shapes = [
+    /* O I L Z S J T */
+    // shapeO
+    ["CC00", "CC00", "CC00", "CC00"],
+    //shapeI
+    ["8888", "F000", "8888", "F000"],
+    //spapeL
+    ["88C0", "2E00", "C440", "E800"],
+    //shapeZ
+    ["C600", "4C80", "C600", "4C80"],
+    //shapeS
+    ["6C00", "8C40", "6C00", "8C40"],
+    //shapeT
+    ["E400", "8C80", "4E00", "4C40"],
+    //shapeJ
+    ["44C0", "E200", "C880", "8E00"]
+]
+
+let currentRow = 0;
+let currentCol = 0;
+let currentShape = {
+    x: 0,
+    y: 0
+};
+
+const board = [];
+
+// 初始化棋盘
+function init() {
+    for (let i = 0; i < BOARD_ROW; i++) {
+        let row = $('<div class="rowDiv">');
+        let board_row = [];
+        for (let j = 0; j < BOARD_COLUMN; j++) {
+            row.append('<span class="spanTetris"></span>');
+            board_row.push(0);
+        }
+        $('.board').append(row);
+        board.push(board_row);
+    }
+    console.log(board);
+}
+
+
+
--- js/tetris.js
+++ js/tetris.js
@@ -1,156 +1,74 @@
-const WIDTH = 20;
-const ROW = 20;
-const COLUMN = 10;
-// 初始化棋盘
-function init() {
-    for (let i = 0; i < ROW; i++) {
-        let row = $('<div class="rowDiv">');
-        for (let j = 0; j < COLUMN; j++) {
-            row.append('<span class="spanTetris"></span>')
+// 画一个方格
+function drawSquare(row, col, color) {
+    let rowDivs = $('.board').children().eq(row).children().eq(col);
+    rowDivs.css('background', color);
+}
+
+// 画一个形状
+function drawShape(y, x, shape) {
+    for (let i = 0; i < 4; i++) {
+        let binaryStr = parseInt(shape[i], 16).toString(2);
+        if (binaryStr.length < 4) {
+            binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
         }
-        $('.board').append(row);
+        for (let j = 0; j < binaryStr.length; j++) {
+            if (binaryStr[j] == 1) {
+                drawSquare(y + i, x + j, SQUARE_COLOR);
+            }
+        }
     }
 }
 
-// 画一个方格
-function drawSquare(x, y, color) {
-    let rowDivs = $('.board').children().eq(x).children().eq(y);
-    rowDivs.css('background', color);
-
+//抹去一个图形
+function eraseShape(y, x, shape) {
+    for (let i = 0; i < 4; i++) {
+        let binaryStr = parseInt(shape[i], 16).toString(2);
+        if (binaryStr.length < 4) {
+            binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
+        }
+        for (let j = 0; j < binaryStr.length; j++) {
+            if (binaryStr[j] == 1) {
+                drawSquare(y + i, x + j, BOARD_COLOR);
+            }
+        }
+    }
 }
 
-//抹去上一个图形
-function allEraseShape(x, y, shape) {
-    console.log(shape);
-    let row;
-    for (let i = 0; i < shape.length; i++) {
-        console.log(i);
-        row = shape[i];
-        console.log(row);
-        for (let j = 0; j < row.length; j++) {
-            let binaryStr = parseInt(row[i], 16).toString(2);
-            if (binaryStr.length < 4) {
-                binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
-            }
-            for (let k = 0; k < binaryStr.length; k++){
-                if (binaryStr[j] == 1){
-                    drawSquare(x + j, y + k, 'orange');
+// 边界验证
+function validate(y, x, shape) {
+    for (let i = 0; i < 4; i++) {
+        let binaryStr = parseInt(shape[i], 16).toString(2);
+        if (binaryStr.length < 4) {
+            binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
+        }
+        for (let j = 0; j < binaryStr.length; j++) {
+            if (binaryStr[j] == 1) {
+                if (y + i < 0 || y + i > 19) {
+                    return false;
+                }
+                if (x + j < 0 || x + j > 9) {
+                    return false;
+                }
+                if(board[y+i][x+j] == 1) {
+                    return false;
                 }
             }
         }
     }
-    /*  for (let i = 0; i < 4; i++) {
-         //console.log(row);
-  
-          let binaryStr = parseInt(row[i], 16).toString(2);
-          //console.log(binaryStr);
-          if (binaryStr.length < 4) {
-              binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
-          }
-          //console.log(binaryStr);
-          for (let j = 0; j < binaryStr.length; j++) {
-              if (binaryStr[j] == 1) {
-                  drawSquare(x + i, y + j, 'orange');
-              }
-          }
-      }   */
+    return true;
 }
 
-function eraseShape(x, y, shape){
-    let row = shape;
-
+// 设置棋盘
+function setBoard(y, x, shape) {
     for (let i = 0; i < 4; i++) {
-        //console.log(row);
-        let binaryStr = parseInt(row[i], 16).toString(2);
-        //console.log(binaryStr);
+        let binaryStr = parseInt(shape[i], 16).toString(2);
         if (binaryStr.length < 4) {
             binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
         }
-        //console.log(binaryStr);
         for (let j = 0; j < binaryStr.length; j++) {
             if (binaryStr[j] == 1) {
-                drawSquare(x + i, y + j, 'orange');
+                board[y + i][x + j] = 1;
             }
         }
     }
 }
-
-// 画一个形状
-function drawShape(x, y, shape) {
-    //console.log(shape);
-    let row = shape;
-
-    for (let i = 0; i < 4; i++) {
-        //console.log(row);
-        let binaryStr = parseInt(row[i], 16).toString(2);
-        //console.log(binaryStr);
-        if (binaryStr.length < 4) {
-            binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
-        }
-        //console.log(binaryStr);
-        for (let j = 0; j < binaryStr.length; j++) {
-            if (binaryStr[j] == 1) {
-                drawSquare(x + i, y + j, 'red');
-            }
-        }
-    }
-}
-
-function clearLotate(x, y, shape) {
-
-    console.log(shape);
-    for (let i = 0; i < 4; i++) {
-        //console.log(row);
-        let row = shape;
-        let binaryStr = parseInt(row[i], 16).toString(2);
-        //console.log(binaryStr);
-        if (binaryStr.length < 4) {
-            binaryStr = Array(4 - binaryStr.length).fill(0).join('') + binaryStr;
-        }
-        //console.log(binaryStr);
-        for (let j = 0; j < binaryStr.length; j++) {
-            if (binaryStr[j] == 1) {
-                drawSquare(x + i, y + j, 'orange');
-            }
-        }
-    }
-}
-
-
-// 变形
-
-
-
-
-
-
-
-
-
-// 所有形状
-const shapes = [
-    /* O I L Z S J T */
-    // shapeO
-    ["CC00", "CC00", "0000", "0000"],
-    //shapeI
-    ["8888", "F000", "8888", "F000"],
-    //spapeL
-    ["88C0", "2E00", "C440", "E800"],
-    //shapeZ
-    ["C600", "4C80", "C600", "4C80"],
-    //shapeS
-    ["6C00", "8C40", "6C00", "8C40"],
-    //shapeT
-    ["E400", "8C80", "4E00", "4C40"],
-    //shapeJ
-    ["44C0", "E200", "C880", "8E00"]
-]
-
-
-// 所有颜色
-const colors = []
-
-// 棋盘状态
-const board = [];
-
-
